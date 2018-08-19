009a833af6dbc808b0becc634a3de929cadcc392
BishoyHanna91 <bishoy@pdx.edu>
2018-08-19 13:38:44 -0700

    C++ files for application

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*   
+   Name: Bishoy Hanna
+   Program Number: 3
+   Number of classes: 6
+   Date: 20/2/2016
+  
+ this is the main class which has an object of the manager, 
+ and he calls his function 
+*/
+# include"manager.h"
+int main()
+{
+  // creating an object of manager class
+  manager m;
+ 
+  // calls his function 
+  m.manageApplication();  
+  return 0;
+}
